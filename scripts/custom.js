fcf9e716f5604db75e466a21ab1a2ab0e460da4e
APDigital <aoifejo.pearson@googlemail.com>
2019-02-26 12:18:38 +0000

    added carousel when thumbnails are clicked

--- scripts/custom.js
+++ scripts/custom.js
@@ -3,9 +3,16 @@ $(document).ready(function () {
         if (this.hash !== "") {
             event.preventDefault();
             var hash = this.hash;
-            if (hash == "#home") {
+            if (hash == "#home", hash == "#content") {
                 $('html, body').animate({
                     scrollTop: 0
+                }, 800, function () {
+                    window.location.hash = hash;
+                });
+            }
+            else if (hash == "#popupContent") {
+                 $('html, body').animate({
+                    scrollTop: ($(hash).offset().top-100)
                 }, 800, function () {
                     window.location.hash = hash;
                 });
@@ -21,3 +28,16 @@ $(document).ready(function () {
     });
 
 });
+
+$(".open").on("click", function(){
+        $(".popup-overlay, .popup-content, .gallery").addClass("active");
+        $("#page, .gallery-page").addClass("hidden");
+
+        var index = $( ".open" ).index( this );
+        $('#popupContent').carousel(index);
+    });
+
+    $(".close").on("click", function(){
+        $(".popup-overlay, .popup-content, .gallery").removeClass("active");
+        $("#page, .gallery-page").removeClass("hidden");
+    });
