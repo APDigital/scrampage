66e4af79a4f70b97845a18114364d738b3672955
AoifePearson@DSTOUTPUT.co.uk <AoifePearson@DSTOUTPUT.co.uk>
2019-06-20 15:45:48 +0100

    Added images

--- scripts/custom.js
+++ scripts/custom.js
@@ -29,15 +29,15 @@ $(document).ready(function () {
 
 });
 
-$(".open").on("click", function(){
-        $(".popup-overlay, .popup-content, .gallery").addClass("active");
-        $("#page, .gallery-page").addClass("hidden");
+// $(".open").on("click", function(){
+//         $(".popup-overlay, .popup-content, .gallery").addClass("active");
+//         $("#page, .gallery-page").addClass("hidden");
 
-        var index = $( ".open" ).index( this );
-        $('#popupContent').carousel(index);
-    });
+//         var index = $( ".open" ).index( this );
+//         $('#popupContent').carousel(index);
+//     });
 
-    $(".close").on("click", function(){
-        $(".popup-overlay, .popup-content, .gallery").removeClass("active");
-        $("#page, .gallery-page").removeClass("hidden");
-    });
+//     $(".close").on("click", function(){
+//         $(".popup-overlay, .popup-content, .gallery").removeClass("active");
+//         $("#page, .gallery-page").removeClass("hidden");
+//     });
