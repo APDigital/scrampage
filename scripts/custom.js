2eccb4514c655d7c8abe8e18142254673074736f
APDigital <aoifejo.pearson@googlemail.com>
2021-05-18 20:39:20 +0100

    Site-update

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,43 +1,38 @@
-$(document).ready(function () {
-    $("a").on('click', function (event) {
+$(document).ready(function() {
+
+    $("#card-content").load("crafting.html");
+    $("a").on('click', function(event) {
         if (this.hash !== "") {
             event.preventDefault();
             var hash = this.hash;
             if (hash == "#home", hash == "#content") {
                 $('html, body').animate({
                     scrollTop: 0
-                }, 800, function () {
+                }, 800, function() {
                     window.location.hash = hash;
                 });
-            }
-            else if (hash == "#popupContent") {
-                 $('html, body').animate({
-                    scrollTop: ($(hash).offset().top-100)
-                }, 800, function () {
+            } else if (hash == "#popupContent") {
+                $('html, body').animate({
+                    scrollTop: ($(hash).offset().top - 100)
+                }, 800, function() {
                     window.location.hash = hash;
                 });
-            }
-            else{
+            } else {
                 $('html, body').animate({
                     scrollTop: ($(hash).offset().top)
-                }, 800, function () {
+                }, 800, function() {
                     window.location.hash = hash;
                 });
             }
         }
     });
-
+    $("#crafting-btn").click(function() {
+        $("#card-content").load("crafting.html");
+    });
+    $("#combat-btn").click(function() {
+        $("#card-content").load("combat.html");
+    });
+    $("#events-btn").click(function() {
+        $("#card-content").load("events.html");
+    });
 });
-
-// $(".open").on("click", function(){
-//         $(".popup-overlay, .popup-content, .gallery").addClass("active");
-//         $("#page, .gallery-page").addClass("hidden");
-
-//         var index = $( ".open" ).index( this );
-//         $('#popupContent').carousel(index);
-//     });
-
-//     $(".close").on("click", function(){
-//         $(".popup-overlay, .popup-content, .gallery").removeClass("active");
-//         $("#page, .gallery-page").removeClass("hidden");
-//     });
